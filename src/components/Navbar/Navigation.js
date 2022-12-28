6afdb14740c2d9ade417623dc5be7943bb05c1a8
ochoachristian <72667987+ochoachristian@users.noreply.github.com>
2022-12-28 19:01:21 +0000

    navbar work

--- src/components/Navbar/Navigation.js
+++ src/components/Navbar/Navigation.js
@@ -16,13 +16,38 @@ export default class Navbar extends Component {
           <div className={styles.location}>Staten Island</div>
         </div> 
 
-        <div className={styles.search}>
-          <div></div>
+        <div className={styles.searchBoxComponent}>
+          <div>
+            <select className={styles.select}>
+              <option value="All">All</option>
+              <option value="Alexa">Alexa</option>
+              <option value="Books">Books</option>
+              <option value="Baby">Baby</option>
+              <option value="Beauty">Beauty</option>
+            </select>
+          </div>
           <div>
             <input type="text" className={styles.searchbar} />
           </div>
-          <div></div>
+          <div className={styles.searchBoxDiv}>
+            <div className={styles.searchIcon}></div>
+          </div>
         </div>
+
+        <div className={styles.signIn}>
+          <div id={styles.hello}>Hello, Sign In</div>
+          <div id={styles.accountList}>Account & List</div>
+        </div>
+        <div className={styles.returns}>
+          <div id={styles.return}>Returns</div>
+          <div id={styles.order}> & Order</div>
+        </div>
+        <div className={styles.cart}>
+          <div className={styles.cartImage}></div>
+          <div className={styles.cartItems}>0</div>
+          <div id={styles.cartText}>Cart</div>
+        </div>
+
       </div>
     )
   }
